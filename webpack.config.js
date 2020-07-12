2b98880720f1637925a3cf423cd1edecca4824bc
gamabuntat <gamabunta@tuta.io>
2020-07-12 21:50:39 +0700

    add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.sass/,
+                test: /\.s[ac]ss$/i,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
