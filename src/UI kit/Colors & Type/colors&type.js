b15f5b701e90c5a66975348c344b7ad5a48f425d
gamabuntat <gamabunta@tuta.io>
2020-08-03 21:21:23 +0800

    add montserrat fonts

--- src/UI kit/Colors & Type/colors&type.js
+++ src/UI kit/Colors & Type/colors&type.js
@@ -1,5 +1,7 @@
 import '@src/UI kit/Colors & Type/colors&type.sass'
 import '@src/fonts/quicksand-regular/fonts.css'
 import '@src/fonts/quicksand-bold/fonts.css'
+import '@src/fonts/montserrat-regular/fonts.css'
+import '@src/fonts/montserrat-bold/fonts.css'
 import '@/node_modules/normalize.css'
 
