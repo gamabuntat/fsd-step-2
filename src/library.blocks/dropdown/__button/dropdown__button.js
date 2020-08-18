59333fd615077b9060de9d9e04adf121c2d0f75b
gamabuntat <gamabunta@tuta.io>
2020-08-18 15:09:46 +0800

    modified dropdown block: add animation

--- src/library.blocks/dropdown/__button/dropdown__button.js
+++ src/library.blocks/dropdown/__button/dropdown__button.js
@@ -1,9 +1,10 @@
 document.getElementById("dropdown__button").addEventListener("click", expandDropdown);
 
 function expandDropdown() {
-    //let expandList = document.getElementById("dropdown__expand-list");
-    document.getElementById("dropdown__expand-list").style.display = "block";
-    //document.getElementById("dropdown__expand-list").style.height = "100px";
+    document.getElementById("dropdown__expand-list").style.height = "159px";
+    document.getElementById("dropdown__expand-list").style.borderBottom = "1px solid #8F8FA0";
+    document.getElementById("dropdown__expand-list").style.borderLeft = "1px solid #8F8FA0";
+    document.getElementById("dropdown__expand-list").style.borderRight = "1px solid #8F8FA0";
     document.getElementById("dropdown__content").style.borderColor = "#8F8FA0";
     document.getElementById("dropdown__content").style.borderBottomRightRadius = "0";
     document.getElementById("dropdown__content").style.borderBottomLeftRadius = "0";
@@ -12,11 +13,16 @@ function expandDropdown() {
     document.getElementById("dropdown__button").addEventListener("click", hideDropdown);
 
     function hideDropdown() {
-        document.getElementById("dropdown__expand-list").style.display = "none";
-        //document.getElementById("dropdown__expand-list").style.height = "0";
+        document.getElementById("dropdown__expand-list").style.height = "0px";
+        document.getElementById("dropdown__expand-list").style.borderBottom = "none";
+        document.getElementById("dropdown__expand-list").style.borderLeft = "1px solid #C7C7CF";
+        document.getElementById("dropdown__expand-list").style.borderRight = "1px solid #C7C7CF";
         document.getElementById("dropdown__content").style.borderColor = "#C7C7CF";
-        document.getElementById("dropdown__content").style.borderBottomRightRadius = "4px";
-        document.getElementById("dropdown__content").style.borderBottomLeftRadius = "4px";
+        function b() {
+            document.getElementById("dropdown__content").style.borderBottomRightRadius = "4px";
+            document.getElementById("dropdown__content").style.borderBottomLeftRadius = "4px";
+        }
+        setTimeout(b, 200);
 
         document.getElementById("dropdown__button").removeEventListener("click", hideDropdown);
         document.getElementById("dropdown__button").addEventListener("click", expandDropdown);
