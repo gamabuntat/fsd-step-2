a10943b7e2bdb25b669d29a8a762fada8ff49e1b
gamabuntat <gamabunta@tuta.io>
2020-10-08 21:59:50 +0800

    continue working on the calendar

--- src/library.blocks/calendar/calendar.js
+++ src/library.blocks/calendar/calendar.js
@@ -1,40 +1,64 @@
 let calendar = document.querySelector('.calendar');
-let today = new Date();
-let thisMounth = new Date(today.getFullYear(), today.getMonth(), 1);
-let lastDate = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
-let weekDay = thisMounth.getDay() - 1;
-if (weekDay == -1) weekDay = 6;
-
 let weeks = calendar.rows;
-//console.log(weeks[0].cells.length);
-//console.log(weeks[weeks.length - 1].cells.length);
+let lastWeek = weeks[weeks.length - 1];
 
-printCalendar(today.getMonth() + 1);
+class MyDate {
+    constructor(date) {
+        this.date = date.getDate();
+        this.month = date.getMonth();
+        this.year = date.getFullYear();
+        this.firstDay = new Date(this.year, this.month, 1).getDay();
+        this.firstDayOfWeek = --this.firstDay == -1 ? 6 : --this.firstDay;
+        this.lastDate = new Date(this.year, this.month + 1, 0).getDate();
+    }
 
-function printCalendar(month, year = today.getFullYear()) {
-    let day = weekDay;
+    whatDate() {
+        console.log([this.firstDayOfWeek, this.lastDate]);
+    }
+
+    callPrintCalendar() {
+        printCalendar.call(this);
+    }
+}
+
+let thisMonth = new MyDate(new Date());
+let secondMonth = new MyDate(new Date(thisMonth.year, thisMonth.month + 1));
+//thisMonth.callPrintCalendar();
+secondMonth.callPrintCalendar();
+
+function printCalendar() {
+    let day = this.firstDayOfWeek;
     let currentDate = 1;
+    let nextMonthWeek = 5;
+    lastWeek.classList.add('calendar__last-week');
 
+    //this month
     for (let week = 1; week < weeks.length; week++) {
         for (; day < weeks[week].cells.length; day++) {
-            if (currentDate > lastDate) break;
+            if (currentDate > this.lastDate) break;
             weeks[week].cells[day].firstElementChild.innerHTML = currentDate;
             currentDate++;
         }
-        if (currentDate > lastDate) break;
+        if (week == 6) {
+            nextMonthWeek = 6;
+            lastWeek.classList.remove('calendar__last-week');
+        }
+        if (currentDate > this.lastDate) break;
         day = 0;
     }
 
-    let lastMonthDate = new Date(thisMounth.getFullYear(), thisMounth.getMonth(), 0).getDate();
-    let prevDay = weekDay - 1;
+    //prev month
+    let lastMonthDate = new Date(this.year, this.month, 0).getDate();
+    let prevDay = this.firstDayOfWeek - 1;
     for (; prevDay >= 0; prevDay--) {
         weeks[1].cells[prevDay].firstElementChild.innerHTML = lastMonthDate;
         lastMonthDate--;
     }
 
+    //next month
     let nextMonthDate = 1;
-    for (; day < weeks[weeks.length - 1].cells.length; day++) {
-        weeks[weeks.length - 1].cells[day].firstElementChild.innerHTML = nextMonthDate;
+    for (; day < weeks[nextMonthWeek].cells.length; day++) {
+        weeks[nextMonthWeek].cells[day].firstElementChild.innerHTML = nextMonthDate;
         nextMonthDate++;
     }
 }
