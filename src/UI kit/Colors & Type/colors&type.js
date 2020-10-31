c2850bfa2baddf87193d5cea1fa75059531f2225
gamabuntat <gamabunta@tuta.io>
2020-10-31 14:49:05 +0800

    add radio buttons

--- src/UI kit/Colors & Type/colors&type.js
+++ src/UI kit/Colors & Type/colors&type.js
@@ -1,5 +1,6 @@
 import '@src/common.blocks/common.sass'
 
+import '@src/fonts/quicksand-light/fonts.css'
 import '@src/fonts/quicksand-regular/fonts.css'
 import '@src/fonts/quicksand-bold/fonts.css'
 import '@src/fonts/montserrat-regular/fonts.css'
