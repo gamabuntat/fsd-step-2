9c95f8643d4e3c42fdf7e48de2ed751bbcd776d6
gamabuntat <gamabunta@tuta.io>
2020-11-06 00:42:18 +0800

    work on the range-slider

--- src/library.blocks/range-slider/range-slider.js
+++ src/library.blocks/range-slider/range-slider.js
@@ -1,30 +1,56 @@
-let slider = document.querySelector('.range-slider');
+let slider = document.querySelector('.range-slider__container');
 let sliderButtonStart = document.querySelector('.range-slider__button_start-range');
 let sliderButtonEnd = document.querySelector('.range-slider__button_end-range');
+let startRange = slider.querySelector('.range-slider__start-range');
+let endRange = slider.querySelector('.range-slider__end-range');
+let priceFrom = document.querySelector('.range-slider__price-from');
+let priceTo = document.querySelector('.range-slider__price-to');
 
-sliderButtonStart.addEventListener('mousedown', setRange);
+let buttonWidth = sliderButtonStart.getBoundingClientRect().width;
+let sliderWidth = slider.getBoundingClientRect().width;
+let priceRange = 15000;
+let priceWeight = priceRange/(sliderWidth - 2*buttonWidth);
 
-function setRange() {
+function calculateThePrice(x) {
+    return Math.trunc(x * priceWeight)
+}
+
+sliderButtonStart.addEventListener('mousedown', setStartRange);
+sliderButtonEnd.addEventListener('mousedown', setEndRange);
+
+init();
+
+function init() {
+    let startButtonCoord = sliderButtonStart.getBoundingClientRect();
+    let endButtonCoord = sliderButtonEnd.getBoundingClientRect();
+    let sliderCoord = slider.getBoundingClientRect();
+    startRange.style.width = startButtonCoord.x - sliderCoord.x + startButtonCoord.width/2 + 'px';
+    endRange.style.width = sliderCoord.right - endButtonCoord.x - startButtonCoord.width/2 + 'px';
+    priceFrom.innerHTML = calculateThePrice(startButtonCoord.x - sliderCoord.x) + '₽';
+    priceTo.innerHTML = calculateThePrice(endButtonCoord.x - sliderCoord.x - buttonWidth) + '₽';
+}
+
+function setStartRange() {
     let button = event.target;
     let buttonCoord = button.getBoundingClientRect();
     let sliderCoord = slider.getBoundingClientRect();
     let endButtonCoord = sliderButtonEnd.getBoundingClientRect();
     let shift = event.clientX - buttonCoord.x;
     let diff = endButtonCoord.x - sliderCoord.x;
+
     moveButton(event.pageX);
 
     function moveButton(pageX) {
         let x = pageX - window.pageXOffset - sliderCoord.x - shift;
         if (x < 0) {
-            button.style.left = 0;
+            x = 0;
         }
         else if (x + buttonCoord.width >= diff) {
-            console.log([x, endButtonCoord.x - sliderCoord.x]);
-            button.style.left = diff - buttonCoord.width + 'px'
+            x = diff - buttonCoord.width;
         }
-        else {
-            button.style.left = x + 'px'
-        }
+        button.style.left = x + 'px';
+        startRange.style.width = x + buttonCoord.width/2 + 'px';
+        priceFrom.innerHTML = calculateThePrice(x) + '₽';
     }
 
     function onMouseMove(event) {
@@ -33,14 +59,56 @@ function setRange() {
 
     document.addEventListener('mousemove', onMouseMove)
 
-    document.addEventListener('mouseup', func);
+    document.addEventListener('mouseup', removeListeners);
 
-    function func() {
+    function removeListeners() {
         document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', func);
+        document.removeEventListener('mouseup', removeListeners);
     }
 }
 
+function setEndRange() {
+    let button = event.target;
+    let buttonCoord = button.getBoundingClientRect();
+    let sliderCoord = slider.getBoundingClientRect();
+    let startButtonCoord = sliderButtonStart.getBoundingClientRect();
+    let shift = event.clientX - buttonCoord.x;
+    let diff = startButtonCoord.right - sliderCoord.x;
+
+    moveButton(event.pageX);
+
+    function moveButton(pageX) {
+        let x = pageX - window.pageXOffset - sliderCoord.x - shift;
+        if (x < diff) {
+            x = diff;
+        }
+        else if (x + buttonCoord.width >= sliderCoord.width) {
+            x = sliderCoord.width - buttonCoord.width;
+        }
+        button.style.left = x + 'px';
+        endRange.style.width = sliderCoord.width - x - buttonCoord.width/2 + 'px';
+        priceTo.innerHTML = calculateThePrice(x - buttonWidth) + '₽';
+    }
+
+    function onMouseMove(event) {
+        moveButton(event.pageX)
+    }
+
+    document.addEventListener('mousemove', onMouseMove)
+
+    document.addEventListener('mouseup', removeListeners);
+
+    function removeListeners() {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', removeListeners);
+    }
+
+}
+
 sliderButtonStart.ondragstart = function() {
+  return false;
+};
+
+sliderButtonEnd.ondragstart = function() {
   return false;
 };
