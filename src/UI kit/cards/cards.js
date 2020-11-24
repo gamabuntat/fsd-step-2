d2d9b60ba5efd7ee23136487c537dfccf14ac9ac
gamabuntat <gamabunta@tuta.io>
2020-11-25 04:34:52 +0800

    add calendar and start room card on cards page

--- src/UI kit/cards/cards.js
+++ src/UI kit/cards/cards.js
@@ -9,15 +9,6 @@ import '@src/fonts/nunito-bold/fonts.css'
 
 import '@src/library.blocks/input/_masked/input_masked.js'
 
-//calendar
-import '@src/library.blocks/calendar/calendar.js'
-import '@src/library.blocks/calendar/__next-button/calendar__next-button.js'
-import '@src/library.blocks/calendar/__prev-button/calendar__prev-button.js'
-import '@src/library.blocks/calendar/__container/calendar__container.js'
-import '@src/library.blocks/calendar/__submit-button/calendar__submit-button.js'
-import '@src/library.blocks/calendar/__cancel-button/calendar__cancel-button.js'
-
-import '@src/library.blocks/date-dropdown/__button/date-dropdown__button.js'
 import '@src/library.blocks/dropdown/dropdown.js'
 import '@src/library.blocks/dropdown/dropdown__observer.js'
 import '@src/common.blocks/dropdown/dropdown_three-guests.js'
