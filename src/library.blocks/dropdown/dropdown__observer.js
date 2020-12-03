20ee3192fc77ddc9af7f20e9f4046c9d32fc3ca5
gamabuntat <gamabunta@tuta.io>
2020-12-03 14:13:45 +0800

    change dropdown observer

--- src/library.blocks/dropdown/dropdown__observer.js
+++ src/library.blocks/dropdown/dropdown__observer.js
@@ -21,24 +21,41 @@ let observer = new MutationObserver(function(mutations) {
             if (dropdown.contains(target)) target = dropdown;
         }
 
-        let sum = Array.from(target.querySelectorAll('.dropdown__counter')).
-            reduce((sum, counter) => +counter.innerHTML + sum, 0);
+        let counters = Array.from(target.querySelectorAll('.dropdown__counter'));
+
+        let sum = counters.reduce((sum, counter) => +counter.innerHTML + sum, 0);
+
+        let lastSum = +counters[counters.length - 1].innerHTML;
+
+        let diff = sum - lastSum;
 
         let partOfString = '';
+        let babyString = '';
+
+        if (diff == 0) {partOfString = ``}
+        else if (diff == 1) {partOfString = `${diff} гость`}
+        else if (diff < 5) {partOfString = `${diff} гостя`}
+        else {partOfString = `${diff} гостей`}
+
+        if (lastSum == 0) {babyString = ''}
+        else if (lastSum == 1) {babyString = `${lastSum} младенец`}
+        else if (lastSum < 5) {babyString =  `${lastSum} младенца`}
+        else {babyString = `${lastSum} младенцев`}
+
+        let result = `${partOfString} ${babyString}`
+        result = result.split(' ');
+        result.length == 4 ? result = `${result[0]} ${result[1]}, ${result[2]} ${result[3]}` : 
+            result = result.join(' ')
 
         if (!sum) {
-            sum = 'Сколько гостей';
+            result = 'Сколько гостей';
             showOrHideCancelButton(target);
-        } else if (sum == 1) {
-            partOfString = ' гость';
+        }
+        else if (sum == 1) {
             if (showCancleButton) showOrHideCancelButton(target);
-        } else if (sum < 5) {
-            partOfString = ' гостя';
-        } else {
-            partOfString = ' гостей';
         }
 
-        target.querySelector('.dropdown__button').firstElementChild.innerHTML = `${sum}${partOfString}`;
+        target.querySelector('.dropdown__button').firstElementChild.innerHTML = `${result}`;
     }
 
     function showOrHideCancelButton(dropdown) {
