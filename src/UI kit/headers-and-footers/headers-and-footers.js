85070e41e1a758d1be438f6cf2dba8f9ed09bfc6
gamabuntat <gamabunta@tuta.io>
2020-12-04 09:06:14 +0800

    finished headers and footers page

--- src/UI kit/headers-and-footers/headers-and-footers.js
+++ src/UI kit/headers-and-footers/headers-and-footers.js
@@ -8,4 +8,6 @@ import '@src/fonts/montserrat-regular/fonts.css'
 import '@src/fonts/montserrat-bold/fonts.css'
 import '@src/fonts/nunito-bold/fonts.css'
 
+import '@library/sub-text-field/sub-text-field.js'
+
 import '@/node_modules/normalize.css'
