b69f518df431a2b1f80fd8f3cfa7dbabbb5b63df
gamabuntat <gamabunta@tuta.io>
2020-12-21 23:37:07 +0800

    responsive room details page

--- src/page/room-details/room-details.js
+++ src/page/room-details/room-details.js
@@ -8,6 +8,7 @@ import '@src/fonts/montserrat-regular/fonts.css'
 import '@src/fonts/montserrat-bold/fonts.css'
 import '@src/fonts/nunito-bold/fonts.css'
 
+import '@library/like-button/like-button.js'
 import '@library/impressions/impressions.js'
 import '@src/library.blocks/dropdown/dropdown.js'
 import '@src/library.blocks/dropdown/dropdown__observer.js'
