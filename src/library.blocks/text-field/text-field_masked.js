dc1274fed926a2044328043f577b99789ceb541c
gamabuntat <gamabunta@tuta.io>
2021-05-26 07:46:48 +0800

    masked text field

--- src/library.blocks/text-field/text-field_masked.js
+++ src/library.blocks/text-field/text-field_masked.js
@@ -1,25 +1,49 @@
 class MaskedTextField {
   constructor(input) {
     this.input = input;
+    this.template = [3, 1, '.', 1, 2, '.', 9, 9, 9, 9];
+    this.index = 0;
     this.bindListeners();
   }
 
   bindListeners() {
-    this.input.addEventListener('keydown', this.getKey.bind(this));
+    this.input.addEventListener('input', this.handleInputInput.bind(this));
   }
 
-  q(e) {
-    if (e.data == '1') e.preventDefault();
-  }
-
-  getKey(e) {
-    const isValidKey = /\d|Backspace|Arrow|Delete/.test(e.code);
-    console.log(isValidKey);
-    if (isValidKey && !e.shiftKey) {
-      console.log(e.code);
-    } else {
-      e.preventDefault();
+  handleInputInput() {
+    const diff = this.getValueIndex() - this.index;
+    if (diff > 0) {
+      if (!this.isDigit()) { 
+        this.backWord();
+        return; 
+      }
+      if (this.template[this.getValueIndex()] == '.') {
+        this.setPoint();
+      }
+    } else if (diff < 0 && this.getTail() == '.') {
+      this.backWord();
     }
+    this.index = this.getValueIndex();
+  }
+
+  getValueIndex() {
+    return this.input.value.length - 1;
+  }
+
+  getTail(o = this.input.value) {
+    return o.length == 1 ? o : o[o.length - 1];
+  }
+
+  isDigit(key = this.getTail()) {
+    return /\d/.test(key);
+  }
+
+  backWord() {
+    this.input.value = this.input.value.slice(0, -1);
+  }
+
+  setPoint(value = this.input.value) {
+    this.input.value  = value.slice(0, -1) + '.' + value[value.length - 1];
   }
 }
 
