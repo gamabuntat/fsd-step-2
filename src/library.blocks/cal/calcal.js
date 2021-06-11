6be20d8ee082d3b246f02fcc6f8f4b61fc348540
gamabuntat <gamabunta@tuta.io>
2021-06-12 04:44:58 +0800

    calendar

--- src/library.blocks/cal/calcal.js
+++ src/library.blocks/cal/calcal.js
@@ -3,20 +3,20 @@ class Cal {
     this.tableContainer = root.querySelector('.js-cal__table-container');
     this.template = this.tableContainer.innerHTML;
     this.tables = root.getElementsByClassName('js-cal__main-table');
-    this.firstTable = this.tables[0];
     this.step = this.tableContainer.getBoundingClientRect().width;
     this.index = 0;
     const now = new Date(root.dataset.date);
     this.year = now.getFullYear();
     this.month = now.getMonth();
     this.fillTable(this.index);
+    this.clearTable(this.index);
   }
 
   static getTail(list) {
     return [...list].pop();
   }
 
-  static getLastN(number) {
+  static getNTail(number) {
     return +Cal.getTail(String(number));
   }
 
@@ -40,8 +40,8 @@ class Cal {
     const r = Cal.increaseAny(coord[1], 6, Math.floor(c / 10));
     return [
       Math.floor(r / 10) + coord[0], 
-      Cal.getLastN(r), 
-      Cal.getLastN(c)
+      Cal.getNTail(r), 
+      Cal.getNTail(c)
     ];
   }
 
@@ -61,19 +61,35 @@ class Cal {
     return new Date(year, month, day).getDay() || 7;
   }
 
-  static getPrevMonthDay(prevLastDate, weekDay) {
-    return Array(weekDay - 1)
+  static getPrevMonthDay(prevLastDate, prevMonthNDay) {
+    return Array(prevMonthNDay)
       .fill(prevLastDate)
       .map((d, idx) => d - idx)
       .reverse();
   }
 
-  static getPresentDay(lastDate) {
-    return [...Array(lastDate).keys()].map((d) => d + 1);
+  static getPresentDay(presentMonthNDay) {
+    return [...Array(presentMonthNDay).keys()].map((d) => d + 1);
   }
 
-  static getNextMonthDay(weekDay) {
-    return Array(7 - weekDay).fill(1).map((d, idx) => d + idx);
+  static getNextMonthDay(nextMonthNDay) {
+    return Array(nextMonthNDay).fill(1).map((d, idx) => d + idx);
+  }
+
+  getPrevMonthNDay(index) {
+    return Cal.getWeekDay(this.year, this.month + index) - 1;
+  }
+
+  getPresentNDay(index) {
+    return Cal.getMonthLastDay(this.year, this.month + index);
+  }
+
+  getNextMonthNDay(index) {
+    return 7 - Cal.getWeekDay(
+      this.year, 
+      this.month + index,
+      Cal.getMonthLastDay(this.year, this.month + index)
+    );
   }
 
   getCell(coord) {
@@ -84,25 +100,29 @@ class Cal {
     const gen = Cal.genCoordsInOrder([index, 0, 0], [index, 5, 6]);
     Cal.getPrevMonthDay(
       Cal.getMonthLastDay(this.year, this.month + index - 1),
-      Cal.getWeekDay(this.year, this.month + index)
+      this.getPrevMonthNDay(index)
     ).forEach((day) => {
       const btn = this.getCell(gen.next().value).firstElementChild;
       btn.innerText = day;
       btn.classList.add('cal__day-btn_prev-month');
     });
-    Cal.getPresentDay(Cal.getMonthLastDay(this.year, this.month + index))
-      .forEach((day) => (
-        this.getCell(gen.next().value).firstElementChild.innerText = day
-      ))
-    Cal.getNextMonthDay(Cal.getWeekDay(
-      this.year, 
-      this.month + index, 
-      Cal.getMonthLastDay(this.year, this.month + index)
-    )).forEach((day) => {
+    Cal.getPresentDay(this.getPresentNDay(index)).forEach((day) => (
+      this.getCell(gen.next().value).firstElementChild.innerText = day
+    ));
+    Cal.getNextMonthDay(this.getNextMonthNDay(index)).forEach((day) => {
       const btn = this.getCell(gen.next().value).firstElementChild;
       btn.innerText = day;
       btn.classList.add('cal__day-btn_next-month');
     });
+  }
+
+  clearTable(index) {
+    const nFilledRows = Math.ceil(
+      (this.getPrevMonthNDay(index) + this.getPresentNDay(index)) / 7
+    );
+    for (let row = 5; row >= nFilledRows; row--) {
+      this.tables[index].rows[row].remove();
+    }
   }
 }
 
