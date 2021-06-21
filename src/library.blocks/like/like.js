72bb106fe131db172dee0699b799fd43c46676bb
gamabuntat <schoshoev@yandex.ru>
2021-06-21 18:37:02 +0800

    like

--- src/library.blocks/like/like.js
+++ src/library.blocks/like/like.js
@@ -2,17 +2,33 @@ class Like {
   constructor(like) {
     this.like = like;
     this.counter = this.like.querySelector('.js-like__counter');
+    this.btn = this.like.querySelector('.js-like__btn');
     this.bindListeners();
   }
 
   bindListeners() {
     this.like.addEventListener('click', this.handleLikeClick.bind(this));
+    this.btn.addEventListener('focus', this.handleBtnFocus.bind(this));
+    this.btn.addEventListener('blur', this.handleBtnBlur.bind(this));
   }
 
   handleLikeClick() {
     this.like.classList.toggle('like_pressed') 
       ? this.counter.innerText -= -1
       : this.counter.innerText -= 1;
+    this.handleBtnFocus();
+  }
+
+  handleBtnFocus() {
+    this.handleBtnBlur();
+    this.like.classList.contains('like_pressed') 
+      ? this.like.classList.add('like_focused-pressed') 
+      : this.like.classList.add('like_focused');
+  }
+
+  handleBtnBlur() {
+    this.like.classList.remove('like_focused-pressed');
+    this.like.classList.remove('like_focused');
   }
 }
 
