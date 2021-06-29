bd76c0475f4923e9b5361054902f3cad13b71a7a
gamabuntat <schoshoev@yandex.ru>
2021-06-30 05:21:42 +0800

    autoimport?

--- src/pages/ui-kit/form-elements/form-elements.js
+++ src/pages/ui-kit/form-elements/form-elements.js
@@ -1,16 +1,10 @@
-//styles
 import '@src/pages/ui-kit/form-elements/form-elements.sass';
 
-//js
-import '@library/text-field/text-field_masked.js';
-import '@library/subscription-text-field/subscription-text-field.js';
-import '@library/dropdown/dropdown.js';
-import '@library/bordered-button/bordered-button.js';
-import '@library/cal/cal.js';
-import '@library/date-dropdown/date-dropdown.js';
-import '@library/filter-date-dropdown/filter-date-dropdown.js';
-import '@library/like/like.js';
-import '@library/range-slider/range-slider.js';
-import '@library/range-label/range-label.js';
-import '@library/check-list/check-list.js';
+const importAll = (r) => {
+  r.keys().filter((k) => !k.includes('like')).forEach((k) => r(k));
+};
+
+/* eslint-disable */
+importAll(require.context('@library/', true, /\.sass$/));
+importAll(require.context('@library/', true, /\.js$/));
 
