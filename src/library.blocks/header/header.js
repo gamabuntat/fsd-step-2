b5a095f73567563f27d1562fa003116c491ccd4e
gamabuntat <schoshoev@yandex.ru>
2021-07-09 12:04:54 +0800

    refactor(header): can be open just one expandable menu

--- src/library.blocks/header/header.js
+++ src/library.blocks/header/header.js
@@ -1,18 +1,64 @@
-class Header {
-  constructor(root) {
-    this.menuButtons = root.querySelectorAll('.js-header__menu-button');
+class EventEmmiter {
+  constructor() {
+    this.handler = (args) => args;
+  }
+  
+  on(handler) {
+    this.handler = handler;
+  }
+
+  emmit(...args) {
+    this.handler(args);
+  }
+}
+
+class MenuButton extends EventEmmiter {
+  constructor(button, idx) {
+    super();
+    this.menuButton = button;
+    this.idx = idx;
+    this.trigger = false;
     this.bindListeners();
   }
 
   bindListeners() {
-    this.menuButtons.forEach((b) => (
-      b.addEventListener('click', (e) => this.handleMenuButtonClick(e)))
-    );
+    this.menuButton
+      .addEventListener('click', () => this.handleMenuButtonClick());
   }
 
-  handleMenuButtonClick(e) {
-    console.log(e);
-    e.target.classList.toggle('header__menu-button_pressed');
+  handleMenuButtonClick() {
+    this.toggleTrigger();
+    this.menuButton.classList.toggle('header__menu-button_pressed');
+    this.trigger && this.emmit(this.idx);
+  }
+
+  closeMenu() {
+    this.trigger = false;
+    this.menuButton.classList.remove('header__menu-button_pressed');
+  }
+
+  toggleTrigger() {
+    this.trigger = !this.trigger;
+  }
+}
+
+class Header {
+  constructor(root) {
+    this.menuButtons = Array.from(
+      root.querySelectorAll('.js-header__menu-button'),
+      (b, idx) => {
+        const c = new MenuButton(b, idx);
+        c.on((args) => this.openMenuHandler(args));
+        return c;
+      }
+    );
+    this.prevOpenIdx = 0;
+  }
+
+  openMenuHandler([idx]) {
+    if (this.prevOpenIdx === idx) { return; }
+    this.menuButtons[this.prevOpenIdx].closeMenu();
+    this.prevOpenIdx = idx;
   }
 }
 
