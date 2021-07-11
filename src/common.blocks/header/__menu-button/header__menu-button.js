e2c4f55f8f412e00f905e17cde4c8865461e6f3b
gamabuntat <schoshoev@yandex.ru>
2021-07-12 00:33:22 +0800

    feat(headers-footers): added narrow footer

--- src/common.blocks/header/__menu-button/header__menu-button.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.header__menu');
-
-document.querySelector('.js-header__menu-button')
-  .addEventListener('click', () => (
-    menu.classList.toggle('header__menu_hidden')
-  ));
-
