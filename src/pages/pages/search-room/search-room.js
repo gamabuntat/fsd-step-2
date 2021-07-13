83e492bd9a3dd71d477474f78bd4186e6e807ed1
gamabuntat <schoshoev@yandex.ru>
2021-07-14 00:20:34 +0800

    feat(search-room|filter): attempts to make a retractable filter

--- src/pages/pages/search-room/search-room.js
+++ src/pages/pages/search-room/search-room.js
@@ -4,3 +4,12 @@ import '@scripts/common.scripts.js';
 
 import './search-room.sass';
 
+class Filter {
+  constructor(root) {
+    this.filter = root;
+    this.filter.classList.toggle('sr-main-filter_expanded');
+  }
+}
+
+new Filter(document.querySelector('.js-sr-main-filter'));
+
