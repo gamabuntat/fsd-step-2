f80435b05a2c0632dbeaf8f96e7f1f58dc07866b
gamabuntat <schoshoev@yandex.ru>
2021-10-01 02:29:42 +0800

    search room refactor BEM #6

--- src/pages/search-room/SearchRoom.js
+++ src/pages/search-room/SearchRoom.js
@@ -7,13 +7,71 @@ class SearchRoom extends BEMBlock {
   }
 
   init() {
-    this.updateElemsMap(['aside-button', 'filter', 'filter-plug']);
-    this.setMods(['search-room__filter_expanded']);
-    this.elemsMap.filter.classList.add(this.mods.filterExpanded);
+    this.updateElemsMap([
+      'aside-button', 'filter',  'filter-plug', 'filter-button'
+    ]);
+    this.setMods(['search-room__filter_expanded', 'search-room--locked']);
+    this.setListeners();
+    this.bindListeners();
+  }
+
+  setListeners() {
+    this.setHandleAsideButtonClick();
+    this.setHandleFilterButtonClick();
+    this.setHandleFilterPlugClic();
+    this.setHandleFilterPlugKeydown();
+  }
+
+  bindListeners() {
+    this.elemsMap.asideButton
+      .addEventListener('click', this.handleAsideButtonClick);
+    this.elemsMap.filterButton
+      .addEventListener('click', this.handleFilterButtonClick);
+    this.elemsMap.filterPlug
+      .addEventListener('click', this.handleFilterPlugClick);
+    this.elemsMap.filterPlug
+      .addEventListener('keydown', this.handleFilterPlugKeydown);
+  }
+
+  setHandleAsideButtonClick() {
+    this.handleAsideButtonClick = () => {
+      this.elemsMap.asideButton.setAttribute('tabindex', '-1');
+      this.showFilter();
+      this.elemsMap.filterButton.focus();
+    };
+  }
+
+  setHandleFilterButtonClick() {
+    this.handleFilterButtonClick = () => {
+      this.elemsMap.asideButton.setAttribute('tabindex', '0');
+      this.hideFilter();
+    };
+  }
+
+  setHandleFilterPlugClic() {
+    this.handleFilterPlugClick = () => {
+      this.elemsMap.asideButton.setAttribute('tabindex', '0');
+      this.hideFilter();
+    };
+  }
+
+  setHandleFilterPlugKeydown() {
+    this.handleFilterPlugKeydown = (e) => {
+      if (e.code == 'Tab' && !e.shiftKey) { 
+        e.preventDefault();
+        this.elemsMap.filterButton.focus(); 
+      }
+    };
   }
 
   showFilter() {
+    this.root.classList.add(this.mods.searchRoomLocked);
+    this.elemsMap.filter.classList.add(this.mods.filterExpanded);
+  }
 
+  hideFilter() {
+    this.root.classList.remove(this.mods.searchRoomLocked);
+    this.elemsMap.filter.classList.remove(this.mods.filterExpanded);
   }
 }
 
